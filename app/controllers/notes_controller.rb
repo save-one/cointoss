c16568ede7bf08d32c27806d4a0547155df7af87
takahiro <nobooknolife16@gmail.com>
2018-09-05 02:01:48 +0900

    alert navbar

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -3,5 +3,22 @@ class NotesController < ApplicationController
   end
 
   def show
+  	@note = Note.find(params[:id])
+  	@targets =Target.joins(:note_targets).where(note_id: @note.id)
+  end
+
+  def create
+  	note = Note.create(user_id: current_user.id)
+  	target1 = Target.create
+  	target2 = Target.create
+  	note_target1 = NoteTarget.create(note_id: note.id, target_id: target1.id)
+  	note_target2 = NoteTarget.create(note_id: note.id, target_id: target2.id)
+  	redirect_to note_path(note)
+  end
+
+  def update
+  end
+
+  def destroy
   end
 end
